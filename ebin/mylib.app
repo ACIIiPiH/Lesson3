2e0279f7426b03a9b06c17ecbcbfb05ed6909e1f
ACIIiPiH <anlev4enko@gmail.com>
2019-07-27 09:32:46 +0300

    First commit

---
+++ ebin/mylib.app
@@ -0,0 +1,7 @@
+{application,mylib,
+             [{description,"An Erlang mylib library"},
+              {vsn,"1"},
+              {modules,[bs01,bs02,bs03,mylib]},
+              {registered,[]},
+              {applications,[kernel,stdlib]},
+              {env,[]}]}.
---
+++ src/bs01.erl
@@ -0,0 +1,10 @@
+-module (bs01).
+-export ([first_word/1]).
+
+%Извлечь из строки первое слово
+
+first_word (BinText) -> first_word(BinText, <<>>).
+
+first_word (<<" ", _/binary>>, Acc) -> Acc;
+first_word (<<C/utf8, Rest/binary>>, Acc) -> first_word (Rest, <<Acc/binary, C/utf8>>);
+first_word(<<>>, Acc) -> Acc.
